86c23b400b9e55e36820b65905b1014831a423fb
Cimerson <stefan.petrovski@gmail.com>
2019-11-01 14:47:57 +0100

    update app with reselect replace card selectors eith memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -17,8 +18,8 @@ const CardIcon = ({ toggleCartHidden, itemCount }) => {
     )
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) =>({
-    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantitiy, 0)
+const mapStateToProps = (state) =>({
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({

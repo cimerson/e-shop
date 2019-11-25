ca8832babf348539cb6b543f1a437043f8107b28
Cimerson <stefan.petrovski@gmail.com>
2019-11-25 16:07:36 +0100

    separating the app into client folder, add express server for stripe backend payment

--- src/redux/directory/directory.selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selecDirectory = state => state.directory;
-
-export const selecDirectorySection = createSelector(
-    [selecDirectory],
-    directory => directory.sections
-)
